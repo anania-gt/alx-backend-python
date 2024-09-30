0db4bf79c41b3e825efe0563d37b5dc682b0ae4e
anania-gt <ananiagebre@outlook.com>
2024-09-30 09:41:54 -0700

    sept303

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
-def add(a: float, b: float) -> float:
+#!/usr/bin/env python3
 
+'''
+    Basic annotations for variables.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Returns sum of two floats.
+    '''
     return a + b
